e287f0f342f55a4a60f71051f74318aa487fd468
jhlee <jhlee@yescnc.co.kr>
2025-11-27 09:02:48 +0900

    chore: CI/CD 웹서비스 포트 정보 수정

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      port: parseInt(env.WEB_PORT || '3000'),
+      port: parseInt(env.SERVER_PORT || '3000'),
       proxy: {
         '/api': {
           target: env.VITE_API_BASE_URL || 'http://localhost:8081',
